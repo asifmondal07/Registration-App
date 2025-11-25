3ca0fa4a1fb8fc21692d0d7f33d1461e43a35b98
asifmonda07 <asikbal7ai@gmail.com>
2025-11-25 12:00:09 +0530

    added

--- frontend/src/component/Header/Header.jsx
+++ frontend/src/component/Header/Header.jsx
@@ -44,7 +44,7 @@ function Header() {
     <header className='bg-amber-100 shadow-md'>
       <Container>
         <div className='flex justify-between items-center py-4'>
-          <h1 className='text-2xl font-bold'>My Blog</h1>
+          <h1 className='text-4xl text-fuchsia-500 font-bold'>E\\V</h1>
           <nav className='flex space-x-4'>
             {navItems
             .filter((item) => item.active)
@@ -52,7 +52,7 @@ function Header() {
               <button
                 key={item.name}
                 to={item.path}
-                className={`text-lg inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full ${item.active ? 'text-blue-500' : 'text-gray-700'}`}
+                className={`text-lg inline-bock px-6 py-2 duration-200 hover:bg-red-200 rounded-full ${item.active ? 'text-blue-500' : 'text-gray-700'}`}
                 onClick={() => {
                   if (item.active) {
                     navigate(item.path)
@@ -76,7 +76,7 @@ function Header() {
               <button 
                 key={firstName}
                 to='/profile'
-                className={`text-lg inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full 
+                className={`text-lg inline-bock px-6 py-2 duration-200 hover:bg-red-200 rounded-full 
                     ${firstName ? 'text-blue-500' : 'text-gray-700'}`}
 
               >
--- frontend/src/component/Header/LogoutBtn.jsx
+++ frontend/src/component/Header/LogoutBtn.jsx
@@ -22,7 +22,7 @@ import { logout } from '../../store/authSlice'
       }
       
     return (
-      <button className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+      <button className='inline-bock px-6 py-2 duration-200 hover:bg-red-200 rounded-full'
       onClick={handleLogout}>Logout</button>
     )
   }
